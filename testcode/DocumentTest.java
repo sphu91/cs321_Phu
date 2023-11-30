d440684f1aaeaa38173389414234555366ee883d
xZephs <117756075+xZephs@users.noreply.github.com>
2023-11-30 14:14:42 -0500

    Update DocumentTest.java

--- testcode/DocumentTest.java
+++ testcode/DocumentTest.java
@@ -1,40 +1,54 @@
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * Tester for Document.
+ * Tester for Document
  * @author M. Phan
  */
+
 public class DocumentTest {
+
+    Applicant a = Applicant.createApplicant("Andrew", "Braddock", 25, 12345, "@gmail");
+    Immigrant i = Immigrant.createImmigrant("Andrew", "Braddock", 25, 12345);
     
     @Test
-    void testCreateDocument(){
-        Applicant a = Applicant.createApplicant("Andrew", "Braddock", 25, 12345, "@gmail");
-        Immigrant i = Immigrant.createImmigrant("Andrew", "Braddock", 25, 12345);
-        
-        assertNull(Document.createDoc(null,null,-1,-1));       //All invalid valid inputs
-        assertNull(Document.createDoc(i,a,200,1));             //Invalid age input
-        assertNull(Document.createDoc(null,a,30,1));           //Invalid immigrant input
-        assertNull(Document.createDoc(i,null,30,1));           //Invalid applicant input
-        assertNull(Document.createDoc(i,a,30,-1000000));       //Invalid id input
-
-        //Successfully create Document
-        assertEquals(Document.createDoc(i, a, 23, 1), Document.class);
-        Document d = Document.createDoc(i, a, 23, 1);
-        assertNotNull(d);
-
+    void testAllInvalid(){
+        assertNull(Document.createDoc(null,null,-1,-1));      //All invalid valid inputs
     }
 
     @Test
-    void testDisplay() {
-        //Objects instantiation for testing
-        Applicant a = Applicant.createApplicant("Andrew", "Braddock", 25, 12345, "@gmail");
-        Immigrant i = Immigrant.createImmigrant("Tony", "Braddock", 25, 12345);
-        Document d = Document.createDoc(i, a, 23, 1);
-
-        //displaying content of Document
-        assertEquals(d.display(), "Applicant: Andrew \nImmigrant: Tony \n Form: 23\nID: 1");
+    void testNullApplicant(){
+        assertNull(Document.createDoc(i,null,30,1));     //Invalid applicant input
     }
+
+    @Test 
+    void testNullImmigrant(){
+        assertNull(Document.createDoc(null,a,30,1));     //Invalid immigrant input
+    }
+
+    @Test 
+    void testInvalidForm(){
+        assertNull(Document.createDoc(i,a,-1,1));        //Invalid form
+    }
+
+    @Test
+    void testInvalidID(){
+        assertNull(Document.createDoc(i,a,30,-1000000));      //Invalid id input
+    }
+
+    @Test 
+    void testSuccess(){
+        //Successfully create Document
+        Document d = Document.createDoc(i, a, 23, 1);
+        assertTrue(d instanceof Document);
+        assertNotNull(d);
+        assertEquals(d.getApplicant(), a);
+    }
+
+    
+
+   
 }
